af4f6ee96c654d267aff1dcef945e83f6f677a97
LimitlessDevv <inyoungchoi26@gmail.com>
2025-01-16 09:57:22 +0900

    Update 05-calico-installation.sh

--- 05-calico-installation.sh
+++ 05-calico-installation.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+cat <<EOF> calico-quay-crd.yaml
+---
+apiVersion: operator.tigera.io/v1
+kind: Installation
+metadata:
+  name: default
+spec:
+  calicoNetwork:
+    ipPools:
+      - blockSize: 26
+        cidr: 192.168.0.0/16
+        encapsulation: VXLANCrossSubnet
+        natOutgoing: Enabled
+        nodeSelector: all()
+  registry: quay.io
+EOF
+
 helm repo add projectcalico https://docs.tigera.io/calico/charts
 helm install calico projectcalico/tigera-operator --version v3.27.5 --namespace tigera-operator --create-namespace
 
